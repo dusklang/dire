56e420c222a7dc83e1ef2d68b2de43ca062e4a07
Zach Wolfe <zachrwolfe@me.com>
2020-12-30 18:29:20 -0600

    Simply IndexCounter

--- src/index_counter.rs
+++ src/index_counter.rs
@@ -1,28 +1,27 @@
-use std::marker::PhantomData;
+use std::ops::AddAssign;
 
 use index_vec::Idx;
 
 #[derive(Debug)]
-pub struct IndexCounter<I: Idx> {
+pub struct IndexCounter<I: Idx + AddAssign<usize>> {
     next_id: I,
-    _phantom: PhantomData<I>,
 }
 
-impl<I: Idx> IndexCounter<I> {
+impl<I: Idx + AddAssign<usize>> IndexCounter<I> {
     pub fn new() -> Self {
-        IndexCounter { next_id: I::from_usize(0), _phantom: PhantomData }
+        IndexCounter { next_id: I::from_usize(0) }
     }
 
     pub fn next(&mut self) -> I {
         let next = self.next_id;
-        self.next_id = I::from_usize(self.next_id.index() + 1);
+        self.next_id += 1;
         next
     }
 
     pub fn len(&self) -> usize { self.next_id.index() }
 }
 
-impl<I: Idx> Default for IndexCounter<I> {
+impl<I: Idx + AddAssign<usize>> Default for IndexCounter<I> {
     fn default() -> Self {
         IndexCounter::new()
     }
